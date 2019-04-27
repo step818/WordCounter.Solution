937d0c0f6dd93d7d16d4364c6bd6a10082dc9349
Stephen Trewick <stephent818@gmail.com>
2019-04-26 17:28:33 -0700

    wordcounterclass with declared private variables

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter;
 
 namespace WordCounter.Tests
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -3,6 +3,45 @@ using System;
 namespace WordCounter
 {
 
+  public class WordCounterClass
+  {
+    private static int _count;
+    private static string _userWord;
+    private static string _userSentence;
+
+    public WordCounterClass(int count, string word, string sentence)
+    {
+      _count = count;
+      _word = word;
+      _sentence = sentence;
+    }
+
+
+    public static bool ValidateString(string userInput)
+    {
+      if (userInput is string)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public static bool SplitSentence(string userSentence)
+    {
+      string[] splitSentence = userSentence.ToLower().Split(' ', ',', '.', '!', ':', ';', '/', '?', '+');
+      if (splitSentence is string[])
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
   public class Program
   {
   	public static void Main()
@@ -14,6 +53,7 @@ namespace WordCounter
   		string userSentence = Console.ReadLine();
       ValidateString(userWord);
   		string[] splitSentence = userSentence.ToLower().Split(' ', ',', '.', '!', ':', ';', '/', '?', '+');
+      SplitSentence(userSentence);
   		foreach(string word in splitSentence)
   		{
   			if(userWord == word)
@@ -24,16 +64,7 @@ namespace WordCounter
   		Console.WriteLine("Your have used " + userWord + " " + count + " times.");
   	}
 
-    public class ValidateString(string userInput)
-    {
-      if (userInput is string)
-      {
-        return true;
-      }
-      else
-      {
-        return false;
-      }
-    }
+
+
   }
 }
